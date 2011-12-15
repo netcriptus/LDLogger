06945541ee517602b0c2bf0ac997bac5113fade6
Fernando Cezar <netcriptus@gmail.com>
2011-12-15 00:14:21 -0200

    Bigger IFEO whitelist

--- lists/associations.py
+++ lists/associations.py
@@ -1,4 +1,4 @@
-associations = {"HKEY_LOCAL_MACHINE\SOFTWARE\Classes\.com": "comfile",
+associations = {"HKEY_LOCAL_MACHINE\SOFTWARE\Classes\.com": "ComFile",
                 "HKEY_LOCAL_MACHINE\SOFTWARE\Classes\comfile\shell\open\command": "\"%1\" %*",
                 "HKEY_LOCAL_MACHINE\SOFTWARE\Classes\.exe": "exefile",
                 "HKEY_LOCAL_MACHINE\SOFTWARE\Classes\exefile\shell\open\command": "\"%1\" %*",
