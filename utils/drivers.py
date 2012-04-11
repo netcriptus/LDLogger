e079669a2a3d27662f22451dbd59c3c01533ab64
Fernando Cezar <netcriptus@gmail.com>
2012-04-11 19:06:16 -0300

    Docstrings inseridos em drivers

--- utils/drivers.py
+++ utils/drivers.py
@@ -11,6 +11,9 @@ from os import getenv
 
 
 def getCompanyName(image_path):
+  """Given a path to a file, it will try to retrieve the Company Name attribute
+  Not all files have this attribute. In this case, the text 'desconhecido' is
+  returned"""
   if image_path.startswith("%"):
     filename = getenv(image_path.split("%")[1].upper()) + "\\" + image_path.split("%")[-1]
   elif image_path.startswith("\\SystemRoot"):
@@ -28,12 +31,15 @@ def getCompanyName(image_path):
   try:
     lang, codepage = GetFileVersionInfo(filename, '\\VarFileInfo\\Translation')[0]
   except:
-    return "Unknown"
+    return "desconhecido"
   strInfoPath = u'\\StringFileInfo\\%04X%04X\\%s' % (lang, codepage, "CompanyName")
   return GetFileVersionInfo(filename, strInfoPath)
 
 
 def parseSC(query_type, raw_info, whitelist):
+  """It parses the result of the SC command, using the information in the raw
+  output to get the display name, service name, company name and path of a
+  driver or service."""
   parsed_sc = []
   for line in raw_info.split("\n"):
     if line.startswith("SERVICE"):
@@ -60,9 +66,11 @@ def parseSC(query_type, raw_info, whitelist):
 
 
 def getServices(whitelist):
-  try:
-    serv = commandHandler.getOutput("sc query type= service")
-  except Exception as err:
+  """Gather the services available in this machine. Returns a list with all
+  services, or a list with an error message if there's an error"""
+  
+  serv = commandHandler.getOutput("sc query type= service")
+  if serv == "":
     errorHandler.logError("sc calling\nThis computer can't execute sc", err)
     return ["Este computador não executa o comando sc. Impossível descobrir serviços."]
   serv = parseSC("SRV", serv, whitelist)
@@ -70,9 +78,11 @@ def getServices(whitelist):
 
 
 def getDrivers(whitelist):
-  try:
-    drvs = commandHandler.getOutput("sc query type= driver")
-  except Exception as err:
+  """Gather the drivers available in this machine. Returns a list with all
+  services, or a list with an error message if there's an error"""
+  
+  drvs = commandHandler.getOutput("sc query type= driver")
+  if drvs == "":
     errorHandler.logError("sc calling\nThis computer can't execute sc", err)
     return ["Este computador não executa o comando sc. Impossível descobrir drivers."]
   drvs = parseSC("DRV", drvs, whitelist)
@@ -80,6 +90,8 @@ def getDrivers(whitelist):
 
 
 def getMountpoints():
+  """Search for mountpoints. Returns None if none is found."""
+  
   suspects = []
   main_key = "HKEY_CURRENT_USER"
   subkey = "Software\Microsoft\Windows\CurrentVersion\Explorer\MountPoints2\%s\shell\%s\command"
@@ -88,14 +100,14 @@ def getMountpoints():
     value = regOps.getRegistryValue(main_key, subkey % (mountpoint, "AutoRun"), "") or\
             regOps.getRegistryValue(main_key, subkey % (mountpoint, "explore"), "") or\
             regOps.getRegistryValue(main_key, subkey % (mountpoint, "open"), "")
-            
-                                    
+                                
     if value:
       suspects.append([smartStr.normalize(mountpoint), smartStr.normalize(value)])
   return suspects or None
 
 
 def searchAutorun():
+  """Scans every local drive looking for autoruns"""
   devices = GetLogicalDriveStrings().split("\\\x00")[:-1]
   autoruns = []
   if "A:" in devices:
@@ -105,11 +117,8 @@ def searchAutorun():
   fixed_devices = [device for device in devices if GetDriveType(device) == DRIVE_FIXED]
   
   for device in fixed_devices:
-    try:
-      device_content = commandHandler.getOutput(["dir", "/a/b", device + "\\"])
-      if "autorun.inf" in device_content or "autorun.exe" in device_content:
-        autoruns.append(device)
-    except Exception as err:
-      continue
+    device_content = commandHandler.getOutput(["dir", "/a/b", device + "\\"])
+    if "autorun.inf" in device_content or "autorun.exe" in device_content:
+      autoruns.append(device)
   return autoruns
     
