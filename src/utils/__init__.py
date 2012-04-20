39cb2bc4a772ff5fff10f5c02c18d1e8aca64b7b
Fernando Cezar <netcriptus@gmail.com>
2012-04-20 16:33:53 -0300

    adding ldlogger module to __init__

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-__all__ = ["regOps", "processes", "services", "drivers", "printer", "commandHandler", "errorHandler", "smartStr"]
+__all__ = ["regOps", "processes", "services", "drivers", "printer", "commandHandler", "errorHandler", "smartStr", "ldlogger"]
