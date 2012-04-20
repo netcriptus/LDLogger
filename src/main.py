172e04e8643c7b90d9e8427cbce72a9ed53dfc05
Fernando Cezar <netcriptus@gmail.com>
2012-04-20 16:33:35 -0300

    Correcting version, saving lists into a dictionary

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ Created by Fernando Cezar on 2011-11-30.
 Copyright (c) 2011 __8bitsweb__. All rights reserved.
 """
 
-VERSION = "1.5 Beta"
+VERSION = "2.0 Beta"
 
 # TODO: I need to verify what libs are necessary inside main.py
 
@@ -16,14 +16,16 @@ from platform import win32_ver, architecture
 from lists import *
 from utils import ldlogger, regOps, processes, services, drivers, printer, commandHandler, errorHandler, smartStr
 
+lists = {}
+
 try:
-  REG_KEYS = REG_KEYS_LIST.REG_KEYS
-  BROWSERS = BROWSERS_LIST.BROWSERS
-  svchost_whitelist = svchostWhitelist.svchost_whitelist
-  winlogon_whitelist = winlogon_whitelist.winlogon_whitelist
-  associations = associations.associations
-  services_whitelist = srv_and_drvs_whitelist.services_whitelist
-  drivers_whitelist = srv_and_drvs_whitelist.drivers_whitelist
+  lists["REG_KEYS"] = REG_KEYS_LIST.REG_KEYS
+  lists["BROWSERS"] = BROWSERS_LIST.BROWSERS
+  lists["svchost_whitelist"] = svchostWhitelist.svchost_whitelist
+  lists["winlogon_whitelist"] = winlogon_whitelist.winlogon_whitelist
+  lists["associations"] = associations.associations
+  lists["services_whitelist"] = srv_and_drvs_whitelist.services_whitelist
+  lists["drivers_whitelist"] = srv_and_drvs_whitelist.drivers_whitelist
 except Exception as err:
   errorHandler.logError("List instantiation", err)
 
@@ -31,8 +33,8 @@ except Exception as err:
 def main(argv):  
 
   # it's just a test
-  ldlogger = ldlogger.LDLogger(VERSION)
-  ldlogger.executeLDLogger()
+  logger = ldlogger.LDLogger(VERSION)
+  logger.executeLDLogger(lists)
 
   return 0
 
